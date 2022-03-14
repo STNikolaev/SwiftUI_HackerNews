9a52c5e07b5167fae1a8c1c6d5a2a6a883da9f6a
STNikolaev <a.o.nikolaev@icloud.com>
2022-03-14 17:39:01 +0300

    initial commit

---
+++ SwiftUI_HackerNews/SwiftUI_HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_HackerNews
+//
+//  Created by Alexandr Nikolaev on 14.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_HackerNews/SwiftUI_HackerNews/SwiftUI_HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HackerNewsApp.swift
+//  SwiftUI_HackerNews
+//
+//  Created by Alexandr Nikolaev on 14.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
